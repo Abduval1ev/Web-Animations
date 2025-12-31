1003f9deb8dc7ae353cc463b899d5c443e5933c0
Abduval1ev <abduvalievabap@gmail.com>
2025-12-31 16:24:28 +0500

    NavBar has been created and styled with GSAP

--- web_animation_moxito/src/App.jsx
+++ web_animation_moxito/src/App.jsx
@@ -1,5 +1,17 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap/gsap-core.js";
+import NavBar from "./components/NavBar";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <main>
+        <NavBar />
+      </main>
+    </>
+  );
 }
 
 export default App;
